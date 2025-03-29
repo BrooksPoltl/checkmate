2588bb0277c15fe2b89b996c0e807357ee0172d3
Brooks Poltl <bpoltl1@gmail.com>
2025-03-29 12:14:15 -0500

    add fixes, hot reloading, and db setup

--- checkmate/src/main/java/com/checkmate/checkmate/CheckMateApplication.java
+++ checkmate/src/main/java/com/checkmate/checkmate/CheckMateApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CheckMateApplication {
-
+	/**
+	 * Main method to run the CheckMate application.
+	 *
+	 * @param args command line arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(CheckMateApplication.class, args);
 	}
