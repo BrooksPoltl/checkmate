2401dc25a28962415eaa2a0d4a3786ffdf299b21
Brooks Poltl <bpoltl1@gmail.com>
2025-04-02 21:37:36 -0500

    add chess util class

---
+++ src/main/java/com/checkmate/utils/ChessUtils.java
@@ -0,0 +1,19 @@
+package com.checkmate.utils;
+
+import com.checkmate.model.Board;
+import com.checkmate.model.Piece;
+
+public class ChessUtils {
+
+    public static boolean isValidMove(Board board, int fromRow, int fromCol, int toRow, int toCol, String currentPlayer) {
+        // TODO: Implement move validation logic
+    }
+
+    public static void applyMove(Board board, int fromRow, int fromCol, int toRow, int toCol) {
+        // TODO: Implement move application logic
+    }
+
+    private static boolean isPathClear(Board board, int fromRow, int fromCol, int toRow, int toCol) {
+        // TODO: Implement path checking logic
+    }
+}
