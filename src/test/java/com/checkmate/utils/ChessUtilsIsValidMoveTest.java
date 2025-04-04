d44cbc38ab2a0983c6d2e4267ee4484f46f7229e
Brooks Poltl <bpoltl1@gmail.com>
2025-04-03 21:05:44 -0500

    Add unit tests for bishop movement validation in ChessUtils

--- src/test/java/com/checkmate/utils/ChessUtilsIsValidMoveTest.java
+++ src/test/java/com/checkmate/utils/ChessUtilsIsValidMoveTest.java
@@ -341,5 +341,201 @@ class ChessUtilsIsValidMoveTest {
         }
     }
     
+    @Nested
+    @DisplayName("Bishop movement validation")
+    class BishopTests {
+        
+        @BeforeEach
+        void setUpBishopTests() {
+            // Clear the board for testing
+            for (int row = 0; row < 8; row++) {
+                for (int col = 0; col < 8; col++) {
+                    board.getSquares()[row][col] = null;
+                }
+            }
+        }
+        
+        @Nested
+        @DisplayName("Basic Bishop movement tests")
+        class BasicBishopMovementTests {
+            
+            @BeforeEach
+            void setUpBishopOnBoard() {
+                // Place a white bishop in the middle of the board for flexible testing
+                board.getSquares()[4][4] = new Piece("bishop", "white", "♗");
+            }
+            
+            @Test
+            @DisplayName("Can move diagonally top-right")
+            void testMoveDiagonallyTopRight() {
+                assertTrue(ChessUtils.isValidMove(board, 4, 4, 1, 7, currentPlayer));
+            }
+            
+            @Test
+            @DisplayName("Can move diagonally top-left")
+            void testMoveDiagonallyTopLeft() {
+                assertTrue(ChessUtils.isValidMove(board, 4, 4, 1, 1, currentPlayer));
+            }
+            
+            @Test
+            @DisplayName("Can move diagonally bottom-right")
+            void testMoveDiagonallyBottomRight() {
+                assertTrue(ChessUtils.isValidMove(board, 4, 4, 7, 7, currentPlayer));
+            }
+            
+            @Test
+            @DisplayName("Can move diagonally bottom-left")
+            void testMoveDiagonallyBottomLeft() {
+                assertTrue(ChessUtils.isValidMove(board, 4, 4, 7, 1, currentPlayer));
+            }
+            
+            @Test
+            @DisplayName("Can move to adjacent diagonal squares")
+            void testMoveToAdjacentDiagonalSquares() {
+                assertTrue(ChessUtils.isValidMove(board, 4, 4, 3, 3, currentPlayer)); // Top-left
+                assertTrue(ChessUtils.isValidMove(board, 4, 4, 3, 5, currentPlayer)); // Top-right
+                assertTrue(ChessUtils.isValidMove(board, 4, 4, 5, 3, currentPlayer)); // Bottom-left
+                assertTrue(ChessUtils.isValidMove(board, 4, 4, 5, 5, currentPlayer)); // Bottom-right
+            }
+            
+            @Test
+            @DisplayName("Maintains color-bound movement")
+            void testMaintainsColorBound() {
+                // A bishop stays on the same color squares throughout the game
+                
+                // Place a bishop on a white square
+                board.getSquares()[4][4] = new Piece("bishop", "white", "♗");
+                
+                // Check that moving diagonally keeps it on the same color
+                // In this case, all moves should end on white squares
+                assertTrue(ChessUtils.isValidMove(board, 4, 4, 5, 5, currentPlayer)); // Still on white
+                assertTrue(ChessUtils.isValidMove(board, 4, 4, 2, 2, currentPlayer)); // Still on white
+                assertTrue(ChessUtils.isValidMove(board, 4, 4, 6, 6, currentPlayer)); // Still on white
+            }
+        }
+        
+        @Nested
+        @DisplayName("Bishop capture tests")
+        class BishopCaptureTests {
+            
+            @BeforeEach
+            void setUpBishopAndOpponentPieces() {
+                // Place a white bishop in the middle of the board
+                board.getSquares()[4][4] = new Piece("bishop", "white", "♗");
+                
+                // Place some opponent pieces in diagonal paths
+                board.getSquares()[2][2] = new Piece("pawn", "black", "♟"); // Top-left
+                board.getSquares()[7][7] = new Piece("knight", "black", "♞"); // Bottom-right
+            }
+            
+            @Test
+            @DisplayName("Can capture diagonally")
+            void testCaptureDiagonally() {
+                assertTrue(ChessUtils.isValidMove(board, 4, 4, 2, 2, currentPlayer)); // Top-left
+                assertTrue(ChessUtils.isValidMove(board, 4, 4, 7, 7, currentPlayer)); // Bottom-right
+            }
+            
+            @Test
+            @DisplayName("Cannot capture pieces of the same color")
+            void testCannotCaptureSameColorPieces() {
+                // Place friendly pieces in the bishop's path
+                board.getSquares()[3][3] = new Piece("pawn", "white", "♙"); // Top-left
+                board.getSquares()[6][6] = new Piece("knight", "white", "♘"); // Bottom-right
+                
+                // Try to "capture" the white pieces
+                assertFalse(ChessUtils.isValidMove(board, 4, 4, 3, 3, currentPlayer));
+                assertFalse(ChessUtils.isValidMove(board, 4, 4, 6, 6, currentPlayer));
+            }
+        }
+        
+        @Nested
+        @DisplayName("Invalid Bishop movement tests")
+        class InvalidBishopMovementTests {
+            
+            @BeforeEach
+            void setUpBishopWithObstacles() {
+                // Place a white bishop in the middle of the board
+                board.getSquares()[4][4] = new Piece("bishop", "white", "♗");
+                
+                // Place obstacles in the bishop's path
+                board.getSquares()[3][3] = new Piece("pawn", "white", "♙"); // Friendly piece on top-left
+                board.getSquares()[6][6] = new Piece("pawn", "black", "♟"); // Opponent on bottom-right
+            }
+            
+            @Test
+            @DisplayName("Cannot move horizontally or vertically")
+            void testCannotMoveHorizontallyOrVertically() {
+                assertFalse(ChessUtils.isValidMove(board, 4, 4, 4, 7, currentPlayer)); // Horizontal
+                assertFalse(ChessUtils.isValidMove(board, 4, 4, 7, 4, currentPlayer)); // Vertical
+            }
+            
+            @Test
+            @DisplayName("Cannot move in non-diagonal paths")
+            void testCannotMoveNonDiagonally() {
+                assertFalse(ChessUtils.isValidMove(board, 4, 4, 6, 7, currentPlayer)); // Not a diagonal
+                assertFalse(ChessUtils.isValidMove(board, 4, 4, 2, 5, currentPlayer)); // Not a diagonal
+            }
+            
+            @Test
+            @DisplayName("Cannot jump over pieces")
+            void testCannotJumpOverPieces() {
+                // Try to jump over the white pawn diagonally
+                assertFalse(ChessUtils.isValidMove(board, 4, 4, 1, 1, currentPlayer));
+                
+                // Try to jump over the black pawn diagonally
+                assertFalse(ChessUtils.isValidMove(board, 4, 4, 7, 7, currentPlayer));
+            }
+            
+            @Test
+            @DisplayName("Cannot move like other pieces")
+            void testCannotMoveInLShape() {
+                // Try to move like a knight
+                assertFalse(ChessUtils.isValidMove(board, 4, 4, 6, 5, currentPlayer));
+                assertFalse(ChessUtils.isValidMove(board, 4, 4, 2, 3, currentPlayer));
+            }
+            
+            @Test
+            @DisplayName("Can stop at opponent piece for capture but not move past it")
+            void testCannotMovePastOpponentPiece() {
+                // Can capture the black pawn at (6,6)
+                assertTrue(ChessUtils.isValidMove(board, 4, 4, 6, 6, currentPlayer));
+                
+                // Cannot move past it to (7,7)
+                assertFalse(ChessUtils.isValidMove(board, 4, 4, 7, 7, currentPlayer));
+            }
+        }
+        
+        @Nested
+        @DisplayName("Black Bishop movement tests")
+        class BlackBishopMovementTests {
+            
+            @BeforeEach
+            void setUpBlackBishop() {
+                // Place a black bishop in the middle of the board
+                board.getSquares()[3][3] = new Piece("bishop", "black", "♝");
+                currentPlayer = "black"; // Switch to black's turn
+            }
+            
+            @Test
+            @DisplayName("Black bishop can move diagonally")
+            void testBlackBishopMovement() {
+                assertTrue(ChessUtils.isValidMove(board, 3, 3, 5, 5, currentPlayer)); // Bottom-right
+                assertTrue(ChessUtils.isValidMove(board, 3, 3, 1, 1, currentPlayer)); // Top-left
+                assertTrue(ChessUtils.isValidMove(board, 3, 3, 1, 5, currentPlayer)); // Top-right
+                assertTrue(ChessUtils.isValidMove(board, 3, 3, 5, 1, currentPlayer)); // Bottom-left
+            }
+            
+            @Test
+            @DisplayName("Black bishop can capture white pieces")
+            void testBlackBishopCapture() {
+                // Place a white piece for the black bishop to capture
+                board.getSquares()[5][5] = new Piece("pawn", "white", "♙");
+                
+                // Black bishop should be able to capture the white pawn
+                assertTrue(ChessUtils.isValidMove(board, 3, 3, 5, 5, currentPlayer));
+            }
+        }
+    }
+    
     // Future test classes for other piece types can be added here
 }
